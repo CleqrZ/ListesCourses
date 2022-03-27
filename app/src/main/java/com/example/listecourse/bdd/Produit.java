23052e41462e12495054641206bed206ece33e53
ggiraudet <gael.giraudet@laposte.net>
2022-03-27 17:45:17 +0200

    menu

---
+++ app/src/main/java/com/example/listecourse/bdd/Produit.java
@@ -0,0 +1,54 @@
+package com.example.listecourse.bdd;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName="Produit")
+public class Produit {
+    @DatabaseField( columnName = "id", generatedId = true )
+    private int idProduit;
+    @DatabaseField
+    private String libelleProduit;
+    @DatabaseField
+    private String quantiter;
+    @DatabaseField
+    private double prixProduit;
+
+
+
+    public Produit() {
+    }
+    public Produit(String libelleProduit,String quantite, double prixProduit) {
+        this.libelleProduit =libelleProduit;
+        this.quantiter =quantite;
+        this.prixProduit  = prixProduit;
+    }
+
+    public String getLibelleProduit() {
+        return libelleProduit;
+    }
+
+    public void setLibelleProduit(String libelleProduit) {
+        this.libelleProduit = libelleProduit;
+    }
+
+    public double getPrixProduit() {
+        return prixProduit;
+    }
+
+    public void setPrixProduit(double prixProduit) {
+        this.prixProduit = prixProduit;
+    }
+
+    public int getIdProduit() {
+        return idProduit;
+    }
+
+    public String getQuantiter() {
+        return quantiter;
+    }
+
+    public void setQuantiter(String quantiter) {
+        this.quantiter = quantiter;
+    }
+}
