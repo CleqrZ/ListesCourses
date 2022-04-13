93c99a0556ed262e6ca54bf24da159529c91a7e6
ggiraudet <gael.giraudet@laposte.net>
2022-04-13 09:47:49 +0200

    corecction Point prix prod

--- app/src/main/java/com/example/listecourse/activity/View_ajout_produit.java
+++ app/src/main/java/com/example/listecourse/activity/View_ajout_produit.java
@@ -17,6 +17,8 @@ import com.example.listecourse.tools.DatabaseLinker;
 import com.j256.ormlite.dao.Dao;
 
 import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class View_ajout_produit extends AppCompatActivity {
 
@@ -63,10 +65,11 @@ public class View_ajout_produit extends AppCompatActivity {
             Log.e("bouton", "Produit value : "+ produit.getLibelleProduit()+" "+produit.getQuantiter()+" "+produit.getPrixProduit());
         }
     }
+    private Pattern prix_regex = Pattern.compile("^[\\d]*['.']{0,1}[\\d]{1,2}$");
     public void modifInfos(){
         String label = editLabel.getText().toString();
         String quantiter = editQuantiter.getText().toString();
-        String prix = editPrix.getText().toString();
+        Matcher matchPrix = prix_regex.matcher(editPrix.getText().toString());
         DatabaseLinker linker = new DatabaseLinker(this);
         Produit produit= new Produit();
         try {
@@ -75,12 +78,18 @@ public class View_ajout_produit extends AppCompatActivity {
                 produit = daoProduit.queryForId(idProduit);
                 produit.setLibelleProduit(label);
                 produit.setQuantiter(quantiter);
-                produit.setPrixProduit(Double.parseDouble(prix));
+                if(matchPrix.matches()){
+                    produit.setPrixProduit(Double.parseDouble(String.valueOf(matchPrix)));
+                }else{
+                    Toast leToast = Toast.makeText(View_ajout_produit.this,
+                            "Remplir touts les champs", Toast.LENGTH_LONG);
+                    leToast.show();
+                }
                 daoProduit.update(produit);
                 Log.i("tesssssssssssssssssst", "ça marche");
             }
             else{
-                if (label.matches("") || quantiter.matches("") || prix.matches("")  ) {
+                if (label.matches("") || quantiter.matches("") || matchPrix.matches()  ) {
                     Log.e("création : ", "erreur");
                     Toast leToast = Toast.makeText(View_ajout_produit.this,
                             "Remplir touts les champs", Toast.LENGTH_LONG);
@@ -89,8 +98,13 @@ public class View_ajout_produit extends AppCompatActivity {
                     produit = new Produit();
                     produit.setLibelleProduit(label);
                     produit.setQuantiter(quantiter);
-                    produit.setPrixProduit(Double.parseDouble(prix));
-                    Log.i("-----------------", "----------------------------------------------------------------------------");
+                    if(matchPrix.matches()){
+                        produit.setPrixProduit(Double.parseDouble(String.valueOf(matchPrix)));
+                    }else{
+                        Toast leToast = Toast.makeText(View_ajout_produit.this,
+                                "Remplir touts les champs", Toast.LENGTH_LONG);
+                        leToast.show();
+                    }
                     daoProduit.create(produit);
                     Intent main = new Intent( View_ajout_produit.this, MainActivity.class);
                     startActivity(main);
